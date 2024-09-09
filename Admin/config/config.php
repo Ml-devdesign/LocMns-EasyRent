b80e9b73fb158bba3f403d081c27f48991722ee8
Leila Mahammed <devmldesign@gmail.com>
2024-09-09 02:33:03 +0200

    Supprime les fichiers ignorés de l'index

--- Admin/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Variables d'environnement pour la connexion à la base de données
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'easyrent');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
